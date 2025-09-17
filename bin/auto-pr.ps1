a4a767cc63c52185644668dfb9b029c0053deb85
anxshi <148721200+anxshi@users.noreply.github.com>
2025-09-17 23:49:05 +0200

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -3,7 +3,7 @@ param(
     [String]$upstream = "anxshi/various-infills:master"
 )
 
-if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
+if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
 $autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
 & $autopr -Dir $dir -Upstream $Upstream @Args
